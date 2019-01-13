f550eda26b61a1dff9d2c58560ad04ba9ff8a84b
Nasimul Hasan <nasimul15@gmail.com>
2019-01-13 14:22:42 +0600

    Update Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -1,17 +1,6 @@
-
-# coding: utf-8
-
-# <a href="https://www.bigdatauniversity.com"><img src="https://ibm.box.com/shared/static/cw2c7r3o20w9zn8gkecaeyjhgw3xdgbj.png" width="400" align="center"></a>
-# 
-# <h1 align="center"><font size="5">Classification with Python</font></h1>
-
-# In this notebook we try to practice all the classification algorithms that we learned in this course.
-# 
 # We load a dataset using Pandas library, and apply the following algorithms, and find the best one for this specific dataset by accuracy evaluation methods.
 # 
 # Lets first load required libraries:
-
-# In[1]:
 
 
 import itertools
@@ -40,66 +29,29 @@ get_ipython().magic(u'matplotlib inline')
 # | Education      | Education of applicant                                                                |
 # | Gender         | The gender of applicant                                                               |
 
-# Lets download the dataset
 
-# In[2]:
-
-
+# download the dataset
 get_ipython().system(u'wget -O loan_train.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/loan_train.csv')
 
 
-# ### Load Data From CSV File  
-
-# In[3]:
-
-
+#Load Data From CSV File  
 df = pd.read_csv('loan_train.csv')
 df.head()
-
-
-# In[4]:
-
 
 df.shape
 
 
-# ### Convert to date time object 
-
-# In[5]:
-
-
+#Convert to date time object 
 df['due_date'] = pd.to_datetime(df['due_date'])
 df['effective_date'] = pd.to_datetime(df['effective_date'])
 df.head()
 
 
-# # Data visualization and pre-processing
-# 
-# 
-
-# Let’s see how many of each class is in our data set 
-
-# In[6]:
-
+# Data visualization and pre-processing
 
 df['loan_status'].value_counts()
 
-
-# 260 people have paid off the loan on time while 86 have gone into collection 
-# 
-
-# Lets plot some columns to underestand data better:
-
-# In[7]:
-
-
-# notice: installing seaborn might takes a few minutes
 get_ipython().system(u'conda install -c anaconda seaborn -y')
-
-
-# In[8]:
-
-
 import seaborn as sns
 
 bins = np.linspace(df.Principal.min(), df.Principal.max(), 10)
@@ -109,10 +61,6 @@ g.map(plt.hist, 'Principal', bins=bins, ec="k")
 g.axes[-1].legend()
 plt.show()
 
-
-# In[9]:
-
-
 bins = np.linspace(df.age.min(), df.age.max(), 10)
 g = sns.FacetGrid(df, col="Gender", hue="loan_status", palette="Set1", col_wrap=2)
 g.map(plt.hist, 'age', bins=bins, ec="k")
@@ -121,12 +69,7 @@ g.axes[-1].legend()
 plt.show()
 
 
-# # Pre-processing:  Feature selection/extraction
-
-# ### Lets look at the day of the week people get the loan 
-
-# In[10]:
-
+# Pre-processing:  Feature selection/extraction
 
 df['dayofweek'] = df['effective_date'].dt.dayofweek
 bins = np.linspace(df.dayofweek.min(), df.dayofweek.max(), 10)
@@ -136,59 +79,30 @@ g.axes[-1].legend()
 plt.show()
 
 
-# We see that people who get the loan at the end of the week dont pay it off, so lets use Feature binarization to set a threshold values less then day 4 
-
-# In[11]:
-
 
 df['weekend'] = df['dayofweek'].apply(lambda x: 1 if (x>3)  else 0)
 df.head()
 
 
-# ## Convert Categorical features to numerical values
-
-# Lets look at gender:
-
-# In[12]:
-
-
+# Convert Categorical features to numerical values
 df.groupby(['Gender'])['loan_status'].value_counts(normalize=True)
 
 
-# 86 % of female pay there loans while only 73 % of males pay there loan
-# 
-
 # Lets convert male to 0 and female to 1:
-# 
-
-# In[13]:
-
-
 df['Gender'].replace(to_replace=['male','female'], value=[0,1],inplace=True)
 df.head()
 
 
-# ## One Hot Encoding  
-# #### How about education?
-
-# In[14]:
-
-
+# One Hot Encoding  
 df.groupby(['education'])['loan_status'].value_counts(normalize=True)
 
 
-# #### Feature befor One Hot Encoding
-
-# In[15]:
-
+#Feature befor One Hot Encoding
 
 df[['Principal','terms','age','Gender','education']].head()
 
 
-# #### Use one hot encoding technique to conver categorical varables to binary variables and append them to the feature Data Frame 
-
-# In[16]:
-
+#Use one hot encoding technique to conver categorical varables to binary variables and append them to the feature Data Frame 
 
 Feature = df[['Principal','terms','age','Gender','weekend']]
 Feature = pd.concat([Feature,pd.get_dummies(df['education'])], axis=1)
@@ -196,58 +110,23 @@ Feature.drop(['Master or Above'], axis = 1,inplace=True)
 Feature.head()
 
 
-# ### Feature selection
-
-# Lets defind feature sets, X:
-
-# In[17]:
-
-
+#Feature selection
 X = Feature
 X[0:5]
 
 
-# What are our lables?
-
-# In[18]:
-
-
+# Finding lables
 y = df['loan_status'].values
 y[0:5]
 
 
-# ## Normalize Data 
-
-# Data Standardization give data zero mean and unit variance (technically should be done after train test split )
-
-# In[19]:
-
-
+#Normalize Data 
 X= preprocessing.StandardScaler().fit(X).transform(X)
 X[0:5]
 
 
-# # Classification 
-
-# Now, it is your turn, use the training set to build an accurate model. Then use the test set to report the accuracy of the model
-# You should use the following algorithm:
-# - K Nearest Neighbor(KNN)
-# - Decision Tree
-# - Support Vector Machine
-# - Logistic Regression
-# 
-# 
-# 
-# __ Notice:__ 
-# - You can go above and change the pre-processing, feature selection, feature-extraction, and so on, to make a better model.
-# - You should use either scikit-learn, Scipy or Numpy libraries for developing the classification algorithms.
-# - You should include the code of the algorithm in the following cells.
-
-# # K Nearest Neighbor(KNN)
-# Notice: You should find the best k to build the model with the best accuracy.  
-# **warning:** You should not use the __loan_test.csv__ for finding the best k, however, you can split your train_loan.csv into train and test to find the best __k__.
-
-# In[20]:
+# Classification 
+# K Nearest Neighbor(KNN)
 
 
 from sklearn.model_selection import train_test_split
@@ -255,15 +134,7 @@ X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random
 print ('Train set:', X_train.shape,  y_train.shape)
 print ('Test set:', X_test.shape,  y_test.shape)
 
-
-# In[21]:
-
-
 from sklearn.neighbors import KNeighborsClassifier
-
-
-# In[22]:
-
 
 from sklearn import metrics
 
@@ -283,10 +154,6 @@ for n in range(1,Ks):
 
 mean_acc
 
-
-# In[23]:
-
-
 plt.plot(range(1,Ks),mean_acc,'g')
 plt.fill_between(range(1,Ks),mean_acc - 1 * std_acc,mean_acc + 1 * std_acc, alpha=0.10)
 plt.legend(('Accuracy ', '+/- 3xstd'))
@@ -295,14 +162,7 @@ plt.xlabel('Number of Nabors (K)')
 plt.tight_layout()
 plt.show()
 
-
-# In[24]:
-
-
 print( "The best accuracy was with", mean_acc.max(), "with k=", mean_acc.argmax()+1)
-
-
-# In[25]:
 
 
 k = 7
@@ -312,10 +172,7 @@ print("Train set Accuracy: ", metrics.accuracy_score(y_train, neigh7.predict(X_t
 print("Test set Accuracy: ", metrics.accuracy_score(y_test, yhat7))
 
 
-# # Decision Tree
-
-# In[26]:
-
+# Decision Tree
 
 from sklearn.tree import DecisionTreeClassifier
 
@@ -323,26 +180,14 @@ loanTree = DecisionTreeClassifier(criterion="entropy", max_depth = 4)
 loanTree # it shows the default parameters
 
 
-# In[27]:
-
-
 loanTree.fit(X_train,y_train)
-
-
-# In[28]:
 
 
 predTree = loanTree.predict(X_test)
 
 
-# In[29]:
-
-
 print (predTree [0:5])
 print (y_test [0:5])
-
-
-# In[30]:
 
 
 from sklearn import metrics
@@ -350,20 +195,10 @@ import matplotlib.pyplot as plt
 print("DecisionTrees's Accuracy: ", metrics.accuracy_score(y_test, predTree))
 
 
-# In[31]:
-
-
 loanTree
-
-
-# In[32]:
-
 
 get_ipython().system(u'conda install -c conda-forge pydotplus -y')
 get_ipython().system(u'conda install -c conda-forge python-graphviz -y')
-
-
-# In[40]:
 
 
 from sklearn.externals.six import StringIO
@@ -371,10 +206,6 @@ import pydotplus
 import matplotlib.image as mpimg
 from sklearn import tree
 get_ipython().magic(u'matplotlib inline')
-
-
-# In[39]:
-
 
 dot_data = StringIO()
 filename = "loantree.png"
@@ -387,32 +218,17 @@ img = mpimg.imread(filename)
 plt.figure(figsize=(100, 200))
 plt.imshow(img,interpolation='nearest')
 
-
-# # Support Vector Machine
-
-# In[41]:
-
-
+# Support Vector Machine
 from sklearn.metrics import classification_report, confusion_matrix
 import itertools
-
-
-# In[42]:
 
 
 from sklearn import svm
 clf = svm.SVC(kernel='rbf')
 clf.fit(X_train, y_train) 
 
-
-# In[43]:
-
-
 yhatSVM = clf.predict(X_test)
 yhatSVM [0:5]
-
-
-# In[44]:
 
 
 def plot_confusion_matrix(cm, classes,
@@ -447,9 +263,6 @@ def plot_confusion_matrix(cm, classes,
     plt.xlabel('Predicted label')
 
 
-# In[45]:
-
-
 # Compute confusion matrix
 cnf_matrix = confusion_matrix(y_test, yhatSVM, labels=['PAIDOFF','COLLECTION'])
 np.set_printoptions(precision=2)
@@ -461,47 +274,26 @@ print (classification_report(y_test, yhatSVM))
 plt.figure()
 plot_confusion_matrix(cnf_matrix, classes=['PAIDOFF','COLLECTION'],normalize= False,  title='Confusion matrix')
 
-
-# In[46]:
-
-
 from sklearn.metrics import f1_score
 f1_score(y_test, yhatSVM, average='weighted')
-
-
-# In[47]:
 
 
 from sklearn.metrics import jaccard_similarity_score
 jaccard_similarity_score(y_test, yhatSVM)
 
-
-# # Logistic Regression
-
-# In[48]:
-
+ # Logistic Regression
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
 LR = LogisticRegression(C=0.01, solver='liblinear').fit(X_train,y_train)
 LR
 
-
-# In[49]:
-
-
 yhatLR = LR.predict(X_test)
 yhatLR
 
 
-# In[50]:
-
-
 yhatLR_prob = LR.predict_proba(X_test)
 yhatLR_prob
-
-
-# In[51]:
 
 
 from sklearn.metrics import classification_report, confusion_matrix
@@ -541,10 +333,6 @@ def plot_confusion_matrix(cm, classes,
     plt.xlabel('Predicted label')
 print(confusion_matrix(y_test, yhatLR, labels=['PAIDOFF','COLLECTION']))
 
-
-# In[52]:
-
-
 # Compute confusion matrix
 cnf_matrix = confusion_matrix(y_test, yhatLR, labels=['PAIDOFF','COLLECTION'])
 np.set_printoptions(precision=2)
@@ -555,23 +343,14 @@ plt.figure()
 plot_confusion_matrix(cnf_matrix, classes=['PAIDOFF','COLLECTION'],normalize= False,  title='Confusion matrix')
 
 
-# In[53]:
-
-
 print (classification_report(y_test, yhatLR))
-
-
-# In[54]:
 
 
 from sklearn.metrics import log_loss
 log_loss(y_test, yhatLR_prob)
 
 
-# # Model Evaluation using Test set
-
-# In[34]:
-
+# Model Evaluation using Test set
 
 from sklearn.metrics import jaccard_similarity_score
 from sklearn.metrics import f1_score
@@ -580,27 +359,16 @@ from sklearn.metrics import log_loss
 
 # First, download and load the test set:
 
-# In[35]:
-
-
 get_ipython().system(u'wget -O loan_test.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/loan_test.csv')
 
 
-# ### Load Test set for evaluation 
-
-# In[36]:
-
-
+# Load Test set for evaluation 
 test_df = pd.read_csv('loan_test.csv')
 test_df.head()
 
 
 # Pre-processing: Feature selection/extraction
-
-# In[55]:
-
-
-##Pre-processing for formatting
+#Pre-processing for formatting
 test_df['due_date'] = pd.to_datetime(df['due_date'])
 test_df['effective_date'] = pd.to_datetime(df['effective_date'])
 test_df['dayofweek'] = test_df['effective_date'].dt.dayofweek
@@ -609,14 +377,7 @@ test_df['Gender'].replace(to_replace=['male','female'], value=[0,1],inplace=True
 test_df.drop(['Unnamed: 0'], axis = 1,inplace=True)
 test_df.drop(['Unnamed: 0.1'], axis = 1,inplace=True)
 
-
-# In[56]:
-
-
 test_df.head()
-
-
-# In[57]:
 
 
 FeatureTDF = test_df[['Principal','terms','age','Gender','weekend']]
@@ -628,21 +389,12 @@ FeatureTDF.rename(columns = {'High School or Below':'high school or below'}, inp
 FeatureTDF.drop(['Master or Above'], axis = 1,inplace=True)
 
 
-# In[58]:
-
-
 XTDF = FeatureTDF
 XTDF.head()
 
 
-# In[59]:
-
-
 yTDF = test_df['loan_status'].values
 yTDF[0:5]
-
-
-# In[60]:
 
 
 XTDF = preprocessing.StandardScaler().fit(XTDF).transform(XTDF)
@@ -651,21 +403,14 @@ XTDF[0:5]
 
 # ACCURACY
 
-# In[61]:
-
-
 yhatTDF = neigh7.predict(XTDF)
 print("TDF set Accuracy: ", metrics.accuracy_score(yTDF, yhatTDF))
 
-
-# In[62]:
 
 
 predTreeTDF = loanTree.predict(XTDF)
 print("DecisionTrees's Accuracy: ", metrics.accuracy_score(yTDF, predTreeTDF))
 
-
-# In[63]:
 
 
 yhatSVMTDF = clf.predict(XTDF)
@@ -673,21 +418,12 @@ from sklearn.metrics import f1_score
 f1_score(yTDF, yhatSVMTDF, average='weighted')
 
 
-# In[64]:
-
-
 from sklearn.metrics import jaccard_similarity_score
 jaccard_similarity_score(yTDF, yhatSVMTDF)
 
 
-# In[65]:
-
-
 yhatLRTDF = LR.predict(XTDF)
 yhatLRTDF_prob = LR.predict_proba(XTDF)
-
-
-# In[66]:
 
 
 LR_JCC = jaccard_similarity_score(yTDF, yhatLRTDF)
@@ -696,10 +432,7 @@ LR_F1 =  f1_score(yTDF,  yhatLRTDF, average='weighted', labels=np.unique(yhatLRT
 print("Logistic Regression Accuracy", LR_JCC, LR_F1, LR_Log)
 
 
-# # Report
-# You should be able to report the accuracy of the built model using different evaluation metrics:
-
-# In[67]:
+#  Report
 
 
 KNN_JCC = jaccard_similarity_score(yTDF, yhatTDF)
@@ -734,19 +467,3 @@ Report = DataFrame(Results, columns= ['Algorithm', 'Jaccard', 'F1-score', 'LogLo
 
 from IPython.display import display, HTML
 display(HTML(Report.to_html(index=False)))
-
-
-# <h2>Want to learn more?</h2>
-# 
-# IBM SPSS Modeler is a comprehensive analytics platform that has many machine learning algorithms. It has been designed to bring predictive intelligence to decisions made by individuals, by groups, by systems – by your enterprise as a whole. A free trial is available through this course, available here: <a href="http://cocl.us/ML0101EN-SPSSModeler">SPSS Modeler</a>
-# 
-# Also, you can use Watson Studio to run these notebooks faster with bigger datasets. Watson Studio is IBM's leading cloud solution for data scientists, built by data scientists. With Jupyter notebooks, RStudio, Apache Spark and popular libraries pre-packaged in the cloud, Watson Studio enables data scientists to collaborate on their projects without having to install anything. Join the fast-growing community of Watson Studio users today with a free account at <a href="https://cocl.us/ML0101EN_DSX">Watson Studio</a>
-# 
-# <h3>Thanks for completing this lesson!</h3>
-# 
-# <h4>Author:  <a href="https://ca.linkedin.com/in/saeedaghabozorgi">Saeed Aghabozorgi</a></h4>
-# <p><a href="https://ca.linkedin.com/in/saeedaghabozorgi">Saeed Aghabozorgi</a>, PhD is a Data Scientist in IBM with a track record of developing enterprise level applications that substantially increases clients’ ability to turn data into actionable knowledge. He is a researcher in data mining field and expert in developing advanced analytic methods like machine learning and statistical modelling on large datasets.</p>
-# 
-# <hr>
-# 
-# <p>Copyright &copy; 2018 <a href="https://cocl.us/DX0108EN_CC">Cognitive Class</a>. This notebook and its source code are released under the terms of the <a href="https://bigdatauniversity.com/mit-license/">MIT License</a>.</p>
